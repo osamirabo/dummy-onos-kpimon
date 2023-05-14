2d38674dfbbf2a5107f1f5845040721d31533f01
osamir <eng4omarsamir@gmail.com>
2023-05-14 15:01:15 +0300

    add go dependency

--- add_go.bash
+++ add_go.bash
@@ -1,3 +1,16 @@
 go get github.com/onosproject/onos-lib-go/pkg/certs
 go get github.com/onosproject/onos-lib-go/pkg/logging
 go get github.com/onosproject/onos-lib-go/pkg/northbound
+go get github.com/osamirabo/dummy-onos-kpimon/pkg/store/measurements
+go get github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1
+go get github.com/onosproject/onos-api/go/onos/kpimon
+go get github.com/onosproject/onos-api/go/onos/topo
+go get github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm_v2_go/pdubuilder
+go get github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm_v2_go/v2/e2sm-kpm-v2-go
+go get github.com/onosproject/onos-ric-sdk-go/pkg/config/app/default
+go get github.com/onosproject/onos-ric-sdk-go/pkg/config/event
+go get github.com/onosproject/onos-ric-sdk-go/pkg/config/registry
+go get github.com/onosproject/onos-ric-sdk-go/pkg/config/utils
+go get github.com/onosproject/onos-ric-sdk-go/pkg/e2/v1beta1
+go get github.com/onosproject/onos-ric-sdk-go/pkg/topo
+
